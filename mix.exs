24b8012babd2805bfda273cdf952089267ae1b63
Laszlo Korte <email@laszlokorte.de>
2026-07-15 18:03:49 +0200

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,8 +7,22 @@ defmodule Galixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: "https://github.com/laszlokorte/galixir",
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
+  end
+
+  defp package() do
+    %{
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/laszlokorte/galixir"}
+    }
+  end
+
+  defp description() do
+    "Elixir Macros implementing Geometric Algebras"
   end
 
   # Run "mix help compile.app" to learn about applications.
